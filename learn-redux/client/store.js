8072baf974f69a3132357c052a8bfb0b4f1c7a0d
david <dpark543107@gmail.com>
2016-11-15 14:16:40 -0600

    connected store with React Router

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,6 +1,6 @@
 import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
-import { broswerHistory } from 'react-router';
+import { browserHistory } from 'react-router';
 
 // import the root reducer
 import rootReducer from './reducers/index';
