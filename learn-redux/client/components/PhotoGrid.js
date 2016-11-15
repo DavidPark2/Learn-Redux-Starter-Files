b4d09d505669bb3c4d7d727ba65b46c20a80d797
david <dpark543107@gmail.com>
2016-11-15 11:56:26 -0600

    created react routes, and some components

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return(
+      <div className="single-grid">
+        I'm a PhotoGrid
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid;
