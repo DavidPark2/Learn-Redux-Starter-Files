074341c709b9104a011683b04fb9c5bf7ac082e5
david <dpark543107@gmail.com>
2016-11-15 14:01:36 -0600

    added a couple reducers

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,10 +1,29 @@
+// Action creater is the function, that puts it together and dispatch it
+// Action is the object inside the return
+
 // increment
-function increment(index) {
+export function increment(index) {
   return {
-    
+    type: 'INCREMENT_LIKES',
+    index
   }
 }
 
 // add comments
+export function addComment(postId, author, comment) {
+  return {
+    type: 'ADD_COMMENT',
+    postId,
+    author,
+    comment
+  }
+}
 
 // remove comments
+export function removeComment(postId, i) {
+  return {
+    type: 'REMOVE_COMMENT',
+    i,
+    postId
+  }
+}
